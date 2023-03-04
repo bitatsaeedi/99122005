c2a5f268c365e586d5af00b0b3e2b6ad3f2b8f42
bitatsaeedi <bitatajiksaeedi@gmail.com>
2023-03-04 12:21:39 +0330

    my first PhpProject

---
+++ project1.php
@@ -0,0 +1,20 @@
+<html>
+     <head>
+        <title>My First Project</title>
+        </head>
+        <meta charset="UTF-8">
+        <body>
+        <?php
+
+        $name  = "bitasaeedi";
+        $age = "21";
+        ?>
+        <h1>Welcome to my first Project</h1>
+        <h2>Hello Everybody!</h4>
+             <h4> My name is 
+            <?php echo ($name) ?><strong> and my age is <?php echo ($age) ?> <?php echo "." ?></h4></strong>
+                
+        </html>
+</body>
+   
+    
