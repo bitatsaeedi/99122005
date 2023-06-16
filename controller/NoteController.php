11e08643c35b1812464b11af3c2c1b3d8218d632
bitatsaeedi <126086853+bitatsaeedi@users.noreply.github.com>
2023-06-16 16:21:01 +0330

    finally

--- controller/NoteController.php
+++ controller/NoteController.php
@@ -3,10 +3,11 @@ require('Database.php');
 $config = require('Config.php');
 $db = new Database($config);
 $id = $_GET['id'];
+$currentUserid = 1;
 $query = "SELECT * FROM notes WHERE id= :id";
 $note = $db->query($query,['id'=>$id])
-            ->fetch(PDO::FETCH_ASSOC);
-
+            ->findOrFail(PDO::FETCH_ASSOC);
+authorize($currentUserid == $note['user_id']);
 require 'views/note.view.php';
 
 ?>
