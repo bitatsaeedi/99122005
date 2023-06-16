60e8a83f85d322ffac195b79cd05ebb9dae55b3c
bitatsaeedi <126086853+bitatsaeedi@users.noreply.github.com>
2023-06-16 16:13:48 +0330

    this is new

--- functions.php
+++ functions.php
@@ -1,7 +1,20 @@
+<<<<<<< HEAD
 <?php 
 function dd($in)
 {
     echo('<pre>');
     var_dump($in);
     die;
+=======
+<?php 
+function dd($in)
+{
+    echo('<pre>');
+    var_dump($in);
+    die;
+}
+function urlIs($value)
+{
+    return $_SERVER['REQUEST_URI']==$value;
+>>>>>>> 1b62c82 (new changes 2)
 }
