74f669812a899b30f20f70db17fc0d7d5dc9e82e
bitatsaeedi <126086853+bitatsaeedi@users.noreply.github.com>
2023-06-16 16:17:21 +0330

    project 5 updated

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php
+require('Response.php');
 require('functions.php');
 require('route.php');
--- route.php
+++ route.php
@@ -1,10 +1,13 @@
 <?php 
 
-$uri = $_SERVER['REQUEST_URI'];
-<<<<<<< HEAD
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+
 $routes = [
     '/' => './controller/HomePageController.php',
     '/about' => './controller/AboutController.php',
+    '/contact' => './controller/ContactController.php',
+    '/notes' => './controller/NotesController.php',
+    '/note' => './controller/NoteController.php'
 ];
 function routeToController($uri, $routes)
 {
@@ -13,24 +16,5 @@ function routeToController($uri, $routes)
     $controller = $controller?  require($controller):abort();
 
 }
-function abort($code='404')
-{
-    http_response_code($code);
-    require("views/{$code}.view.php");
-}
 
 routeToController($uri, $routes);
-=======
-$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
-
-$routes = [
-    '/' => './controller/HomePageController.php',
-    '/about' => './controller/AboutController.php',
-    '/contact' => './controller/ContactController.php'
-    '/contact' => './controller/ContactController.php',
-    '/notes' => './controller/NotesController.php',
-    '/note' => './controller/NoteController.php'
-];
-function routeToController($uri, $routes)
-{
->>>>>>> f7d9669 (updated)
